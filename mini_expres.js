c73ec9752b8bb87fd6d63c9bd91109196a852bfb
henri.falves <henri.falves@hotmail.com>
2018-01-08 22:32:19 -0200

    mini_express

---
+++ mini_expres.js
@@ -0,0 +1,22 @@
+var http = require('http');
+
+
+
+var server = http.createServer(function(request, response){
+    
+    var router = []
+    router['/produtos'] = '<h1>Produtos</h1>'
+    router['/'] = '<h1>Home</h1>'
+
+    console.log(router);
+    if(router[request.url])
+        response.end(router[request.url]);
+    else{
+        response.writeHead(404)
+        response.end('404');
+    }
+        
+});
+
+server.listen(3001, 'localhost');
+console.log(`server up`);
