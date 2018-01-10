3c1d18b1ea7bea51b237c9c3a3e398b93a539a85
henri.falves <henri.falves@hotmail.com>
2018-01-09 22:56:16 -0200

    commit

--- config-express.js
+++ config-express.js
@@ -1,7 +1,6 @@
 module.exports = function(){
     const express = require('express')
     const app = express();
-    
     app.listen(3000, function(){
         console.log(`Escutando a porta 3000`);
     });
