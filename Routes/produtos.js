0e00228e9dc328ede7b9cb32be733108688bb710
henri.falves <henri.falves@hotmail.com>
2018-01-11 22:08:52 -0200

    commit do insert

--- Routes/produtos.js
+++ Routes/produtos.js
@@ -1,25 +1,39 @@
 module.exports = function(app) {
     const pagina = 'Produtos'
     const conn = require('../config-connection.js')
+    const ProdutosDAO = require('../DAO/produtos')
 
     app.get('/produtos', function(req,res){
         const connection = conn.connection()
+        const produtosDAO = new ProdutosDAO(connection);
 
-        const produtos = connection.query('SELECT * FROM Produtos', function(err, result){
-            res.render(`produtos/lista`, {pagina: pagina, produtos: result})
+        produtosDAO.Get((err, result) => {
+            res.render('produtos/lista', {pagina: pagina, produtos: result})
         });
-        connection.end()
-    })
+    });
     
+    app.get('/produtos/form', function(req,res){
+        res.render('produtos/form', {pagina: pagina})
+    })
+
     app.get('/produtos/:id', function(req, res){
         const connection = conn.connection()
+        const produtosDAO = new ProdutosDAO(connection);
 
-        const id = req.params.id
-        console.log(conn.mysql)
-        const produtos = connection.query('SELECT * FROM Produtos WHERE ID= ?',id, function(err, result){
+        produtosDAO.GetById(req.params.id, (err, result) =>
             res.render(`produtos/lista`, {pagina: pagina, produtos: result})
+        );
+    });
+
+    app.post('/produtos', function(req, res){
+        const connection = conn.connection()
+        const produtosDAO = new ProdutosDAO(connection);
+        
+        const produto = req.body
+
+        produtosDAO.Insert(produto, function (err){
+            res.send(produto)
         });
-        connection.end()
     });
 
     return app;
--- config-express.js
+++ config-express.js
@@ -1,16 +1,24 @@
 module.exports = function(){
     const express = require('express')
     const app = express();
-    
+    const bodyParser = require('body-parser')
     //set
     app.set('view engine', 'ejs')
 
-    //Middleware
     app.use(express.static('./public'))
-
+    app.use(bodyParser.urlencoded({extended:true}));
     //Rotas
     require('./Routes/home')(app)
     require('./Routes/produtos.js')(app)
     
+    //Middleware
+    app.use(function(req,res,next){
+        res.status(404).render(err).next()
+    });
+
+    app.use(function(err,req,res,next){
+        res.status(500).render(err).next()
+    });
+
     return app;
 }
