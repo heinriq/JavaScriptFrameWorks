c7c4cd71dab67fc915751866d84a8fe456116277
henrifalves <henri.falves@hotmail.com>
2018-01-19 02:54:14 -0200

    todos os arquivos

---
+++ NodeJS/CRUD-mysql/Infra/config-connection.js
@@ -0,0 +1,13 @@
+const mysql = require('mysql')
+
+function connection(){
+
+    return mysql.createConnection({
+        host:'localhost',
+        user:'root',
+        password: 'caelum',
+        database: 'casaDoCodigo'
+    }); 
+}
+
+module.exports = () => connection
