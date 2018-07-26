950729e2a110dba9e3611569229ace9d10b4187d
Henrique Alves <henri.falves@hotmail.com>
2018-07-26 00:43:47 -0300

    git ignore e exclusao de node_modules

--- Angular/PicList/src/app/app.component.ts
+++ Angular/PicList/src/app/app.component.ts
@@ -1,5 +1,4 @@
-import { Component, NgModule } from '@angular/core';
-import { Http } from '@angular/http'; 
+import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   template: '<router-outlet></router-outlet>',
